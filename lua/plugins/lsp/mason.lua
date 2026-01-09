60acecb656f999435ba836839f085e1c8441010f
Valera Tsyplakov <webvalera96@yandex.ru>
2026-01-09 21:50:14 +0300

    - add conform plugin for running linters - add mason lsp settings - remove ray-x-go.lua

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -1,16 +1,33 @@
 return {
-  "mason-org/mason.nvim",
-  opts = {
-    ui = {
-      icons = {
-        package_installed = "✓",
-        package_pending = "➜",
-        package_uninstalled = "✗"
-      }
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗"
+        }
+      },
+      PATH = "prepend"
+
+    }
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = {
+        -- LSP
+        "gopls", "lua-language-server",
+        -- DAP
+        -- Formatters
+        "stylua", "goimports", "gofmt",
+        -- Linters
+      },
     },
-    ensure_installed = {
-      "gopls",
-      "lua-language-server"
+    dependencies = {
+        { "mason-org/mason.nvim", opts = {} },
+        "neovim/nvim-lspconfig",
     }
   }
 }
