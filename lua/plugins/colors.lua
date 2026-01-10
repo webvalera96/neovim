b0fbba6378ccc7c3be71d46a9167b89761daf6e8
Valera Tsyplakov <webvalera96@yandex.ru>
2026-01-10 04:21:04 +0300

    - switch to dracula theme (more standard)

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,11 +1,13 @@
 local function enable_transparency()
-  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 end
 
 return {
-  "folke/tokyonight.nvim",
-  config = function()
-    vim.cmd.colorscheme "tokyonight"
-    enable_transparency()
-  end
+	"binhtran432k/dracula.nvim",
+	config = function()
+		vim.cmd.colorscheme("dracula")
+		enable_transparency()
+	end,
+	priority = 1000,
+	opts = {},
 }
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,7 +2,7 @@ return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	opts = {
-		theme = "tokyonight",
+		theme = "dracula",
 		options = {
 			disabled_filetypes = {
 				statusline = { "neo-tree", "Avante", "AvanteInput", "gitsigns-blame", "OverseerList", "OverseerOutput" },
