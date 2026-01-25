350bcb681465810e54b3c85b33dc23a219c1b859
Valera Tsyplakov <webvalera96@yandex.ru>
2026-01-25 11:11:55 +0300

    - fix golang completion, switch to nushell

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -1,32 +1,62 @@
 return {
-  {
-    "mason-org/mason.nvim",
-    opts = {
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
-        },
-      },
-      PATH = "prepend",
-    },
-  },
-  {
-    "mason-org/mason-lspconfig.nvim",
-    opts = {
-      automatic_enable = {
-        exclude = {
-          "rust_analyzer"
-        }
-      },
-      ensure_installed = {
-        "gopls", "lua_ls", "rust_analyzer"
-      },
-    },
-    dependencies = {
-      { "mason-org/mason.nvim", opts = {} },
-      "neovim/nvim-lspconfig",
-    }
-  }
+	{
+		"mason-org/mason.nvim",
+		opts = {
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+			PATH = "prepend",
+		},
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		opts = {
+			automatic_enable = {
+				exclude = {
+					"rust_analyzer",
+				},
+			},
+			ensure_installed = {
+				"gopls",
+				"lua_ls",
+				"rust_analyzer",
+			},
+		},
+		dependencies = {
+			{ "mason-org/mason.nvim", opts = {} },
+			"neovim/nvim-lspconfig",
+		},
+		handlers = {
+			gopls = function()
+				require("lspconfig").gopls.setup({
+					capabilities = {
+						textDocument = {
+							hoverProvider = true, -- явно включи hover
+						},
+					},
+					settings = {
+						gopls = {
+							gofumpt = true, -- синхронизируй с formatter
+							completeUnimported = true,
+							usePlaceholders = true,
+							hints = {
+								assignVariableTypes = true,
+								compositeLiteralFields = true,
+								constantValues = true,
+								parameterNames = true,
+							},
+							analyses = {
+								unusedparams = true,
+								shadow = true,
+							},
+						},
+					},
+				})
+			end,
+		},
+	},
 }
--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,9 +1,9 @@
 return {
-  "akinsho/toggleterm.nvim",
-  version = "*",
-  opts = {
-    close_on_exit = false,
-    open_mapping = [[<leader>`]],
-    shell = "zsh -i",
-  },
+	"akinsho/toggleterm.nvim",
+	version = "*",
+	opts = {
+		close_on_exit = false,
+		open_mapping = [[<leader>`]],
+		shell = "nu",
+	},
 }
