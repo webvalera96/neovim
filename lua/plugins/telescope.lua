bacc7a617e8de52c867d1d5389f8c8a7c388a96f
Valera Tsyplakov <webvalera96@yandex.ru>
2026-01-25 13:03:16 +0300

    - fix telescop keybinds

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,19 +1,16 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  config = function()
-    local builtin = require("telescope.builtin")
-    vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
-    vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
-    vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
-    vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
-  end,
-  keys = {
-    { "<leader>fs", "<cmd>Telescope diagnostics<CR>", { desc = "Telescope diagnostics" } },
-  },
-  opts = {
-    layout_config = {
-      vertical = { width = 0.5 }
-    }
-  }
+	"nvim-telescope/telescope.nvim",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local builtin = require("telescope.builtin")
+		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+	end,
+	opts = {
+		layout_config = {
+			vertical = { width = 0.5 },
+		},
+	},
 }
